d73276efc551d9b6c126abf790fbe0e6acb93e24
alsdk1676 <alsdk6761@gmail.com>
2025-03-23 21:05:20 +0900

    auction-payment-main 수정

--- src/main/webapp/assets/js/auction/auction-payment-main.js
+++ src/main/webapp/assets/js/auction/auction-payment-main.js
@@ -6,15 +6,12 @@ const phoneError = document.getElementById("phone-error");
 const emailError = document.getElementById("email-error");
 
 function validateForm() {
-    // 입력값을 가져옵니다.
     const address = document.getElementById("address").value.trim();
     const phone = document.getElementById("phone").value.trim();
     const email = document.getElementById("email").value.trim();
 
-    let isValid = true; // 유효성 체크 변수 초기화
+    let isValid = true; 
 
-
-    // 필수 항목이 비어있을 경우 오류 메시지를 띄우고 유효성 체크를 위해 isValid를 false로 설정
     if (address === "") {
         addressError.style.display = "inline";
         addressError.style.color = "var(--color-warning)";
@@ -33,26 +30,20 @@ function validateForm() {
         isValid = false;
     }
 
-    // 유효성 체크 후 버튼 스타일 변경
     if (isValid && checkUser.checked && checkPayment.checked) {
         paymentButton.classList.add("active");
-        paymentButton.style.cursor = "pointer"; // 버튼 클릭 가능
     } else {
         paymentButton.classList.remove("active");
-        paymentButton.style.cursor = "not-allowed"; // 버튼 클릭 불가
     }
 
-    return isValid; // 폼 제출 여부 결정
+    return isValid;
 }
 
 const changePaymentButton = () => {
-    // 체크박스가 모두 체크되었을 때 결제 버튼 스타일 변경
     if (checkUser.checked && checkPayment.checked) {
         paymentButton.classList.add("active");
-        paymentButton.style.cursor = "pointer"; // 버튼 클릭 가능
     } else {
         paymentButton.classList.remove("active");
-        paymentButton.style.cursor = "not-allowed"; // 버튼 클릭 불가
     }
 };
 
@@ -61,6 +52,24 @@ checkPayment.addEventListener("change", changePaymentButton);
 
 paymentButton.addEventListener("click", function(event) {
     if (!validateForm()) {
-        event.preventDefault(); // 폼이 유효하지 않으면 제출을 막음
+        event.preventDefault();
+    }
+});
+
+document.getElementById("address").addEventListener("input", function() {
+    if (this.value.trim() !== "") {
+        addressError.style.display = "none";
+    }
+});
+
+document.getElementById("phone").addEventListener("input", function() {
+    if (this.value.trim() !== "") {
+        phoneError.style.display = "none";
+    }
+});
+
+document.getElementById("email").addEventListener("input", function() {
+    if (this.value.trim() !== "") {
+        emailError.style.display = "none";
     }
 });
