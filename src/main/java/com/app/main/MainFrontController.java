b3db3b843ce2d2e5b26a04581cb6d17796925771
Dgeon1008 <Dcode0971@gmail.com>
2025-03-24 09:06:16 +0900

    3/24

---
+++ src/main/java/com/app/main/MainFrontController.java
@@ -0,0 +1,46 @@
+package com.app.main;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.app.Result;
+
+public class MainFrontController extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=utf-8;");
+		
+		String target = req.getRequestURI().replace(req.getContextPath() + "/", "").split("\\.")[0];
+		Result result = null;
+		
+		
+		if(target.equals("mypage/mypage-contact-artist-list")) {
+		}else if(target.equals("mypage/mypage-contact-artist-write")) {
+		}else if(target.equals("auction/auction-payment-main")) {
+		}else {
+//			전부 404
+		}
+		
+		
+		
+		if(result != null) {
+			if(result.isRedirect()) {
+				resp.sendRedirect(result.getPath());
+			}else {
+				req.getRequestDispatcher(result.getPath()).forward(req, resp);
+			}
+		}
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+
+}
