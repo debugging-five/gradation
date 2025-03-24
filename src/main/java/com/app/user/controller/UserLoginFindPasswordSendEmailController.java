a17992ffc74a196a9612c6fc54968d5dd9459979
sehyunf <ssam010925@gmail.com>
2025-03-24 17:00:36 +0900

    day08 17 00

--- src/main/java/com/app/user/controller/UserLoginFindPasswordSendEmailController.java
+++ src/main/java/com/app/user/controller/UserLoginFindPasswordSendEmailController.java
@@ -5,19 +5,16 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import com.app.Action;
 import com.app.Result;
 
-public class UserLoginFindPasswordController implements Action {
+public class UserLoginFindPasswordSendEmailController implements Action {
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		Result result = new Result();
-		HttpSession session = req.getSession();
 		
-		session.setAttribute("mailCode", "");
 		result.setPath("login-find-password.jsp");
 		return result;
 	}
