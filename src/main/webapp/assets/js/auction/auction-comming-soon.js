b0b840937c4a979b802df3e2db1b9785a05cf0c9
alsdk1676 <alsdk6761@gmail.com>
2025-03-24 16:31:12 +0900

    auction-comming-soon 수정

--- src/main/webapp/assets/js/auction/auction-comming-soon.js
+++ src/main/webapp/assets/js/auction/auction-comming-soon.js
@@ -1,5 +1,6 @@
 const listButton = document.getElementById("list-button");
 
+/* 경매 메인 페이지로 이동 */
 listButton.addEventListener("click", () => {
-	location.href = 'auction-main.auction'; 
+	location.href = "auction-main.auction"; 
 });
