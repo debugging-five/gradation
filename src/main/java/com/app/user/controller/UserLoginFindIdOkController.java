e02fb944e0512e3e2e2e31fb0ff58232fb608c5c
sehyunf <ssam010925@gmail.com>
2025-03-24 15:13:28 +0900

    day08 15 13

--- src/main/java/com/app/user/controller/UserLoginFindIdOkController.java
+++ src/main/java/com/app/user/controller/UserLoginFindIdOkController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.app.Action;
 import com.app.Result;
@@ -14,6 +15,9 @@ public class UserLoginFindIdOkController implements Action {
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		Result result = new Result();
+		HttpSession session = req.getSession();
+		
+		session.setAttribute("isComplete", "true");
 		
 		result.setRedirect(true);
 		result.setPath("login-find-id.user");
--- src/main/webapp/assets/js/login/login-find-id.js
+++ src/main/webapp/assets/js/login/login-find-id.js
@@ -1,6 +1,10 @@
-/**
- * 
- */
+//	인증값
+let mailCode = document.querySelector("#mail-code");
+let checkEmail = document.querySelector("#check-email");
+//	input 초기화
+let userName = document.querySelector("#user-name");
+let userEmail = document.querySelector("#user-email");
+let userCode = document.querySelector("#user-code");
 
 //	아이디 찾기 완료 시 리다이렉트
 let isComplete = document.querySelector("#is-complete");
@@ -8,21 +12,102 @@ if(isComplete.value == "true"){
 	const idAccount = document.querySelector("#id-account");
 	idAccount.style.display = 'block';
 }
-
-const findIdButton = document.querySelector("#find-id-button");
-const findIdCloseButton = document.querySelector("#find-id-close-button");
-
 //	아이디 조회창
 const idAccount = document.querySelector("#id-account")
+//	삭제하기
 
-let userName = document.querySelector("#user-name");
+// 	이메일 인증 전송 버튼
+const emailCheckButton = document.querySelector("#email-check-button");
+
+//	인증번호 확인 버튼
+const codeCheckButton = document.querySelector("#code-check-button");
+
+//	아이디 조회 버튼
+const findIdButton = document.querySelector("#find-id-button");
+
+//	아이디 창 닫기 버튼
+const findIdCloseButton = document.querySelector("#find-id-close-button");
+
+//	오류 메세지
+const needNameInput = document.querySelector("#need-name-input");
+const needEmailCheck = document.querySelector("#need-email-check");
+
+//	완료 메세지
+const emailComplete = document.querySelector("#email-complete");
+const emailCheckComplete = document.querySelector("#email-check-compleate");
+const needEmailCheckValidate = document.querySelector("#need-email-check-validate");
+const needEmailValidate = document.querySelector("#need-email-validate");
 
 //	아이디 찾기 type : submit 변경 조건
 const update = () => {
 //	필드 재정의
-	let name
+	userName = document.querySelector("#user-name");
+	userEmail = document.querySelector("#user-email");
+	userCode = document.querySelector("#user-code");
+}
+
+//	총합검증
+const checkAll = () => {
+	if(userName.value == "") {
+		needNameInput.classList.add("need-write");
+	}else {
+		needNameInput.classList.remove("need-write");
+	}
+	if(checkEmail.value == "" || checkEmail.value == "false") {
+		needEmailCheck.classList.add("need-write");
+		needEmailCheckValidate.classList.remove("need-write");
+	}else {
+		needEmailCheck.classList.remove("need-write");		
+		needEmailCheckValidate.classList.remove("need-write");
+	}
+	
 	
 }
+// 이메일 발송 완료, 미완료
+const emailCheckDesign = () => {
+	if(mailCode.value == "") {
+		emailCheckButton.textContent = "이메일 인증"
+		emailCheckButton.style.backgroundColor = "var(--color-gray500)";
+	}else if(mailCode.value == "false") {
+		console.log("유효하지 않은 사용자")
+		needEmailValidate.classList.add("need-write");
+	}else {		
+		emailCheckButton.textContent = "이메일 재발송"
+		emailCheckButton.style.backgroundColor = "var(--color-primary)";
+		emailComplete.classList.add("complete-write");
+	}
+}
+
+// 이메일 발송 버튼 클릭
+emailCheckButton.addEventListener("click",() => {
+	if(userEmail.value && userName.value) {
+		location.href =
+			'login-find-send-email.user?userName=' + encodeURIComponent(userName.value) +
+			'&userEmail=' + encodeURIComponent(userEmail.value) +
+			'&userCode=' + encodeURIComponent(userCode.value);							
+	}else {
+	}
+});
+
+// 인증번호 확인 버튼 클릭
+codeCheckButton.addEventListener("click",() => {
+	needEmailCheck.classList.remove("need-write");
+	if(userCode.value == mailCode.value && userCode.value != "") {
+		isComplete.value = "true";
+		findIdButton.type = "submit";
+		codeCheckButton.style.backgroundColor = 'var(--color-primary)'
+		emailCheckComplete.classList.add("complete-write");
+		needEmailCheckValidate.classList.remove("need-write");
+	}else {
+		findIdButton.type = 'button';
+		codeCheckButton.style.backgroundColor = 'var(--color-gray500)'
+		emailCheckComplete.classList.remove("complete-write");
+		needEmailCheckValidate.classList.add("need-write");
+	}
+})
+
+// 메인검증
+emailCheckDesign();
 
 
 findIdButton.addEventListener("click",() => {
