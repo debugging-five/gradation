c8180233f64a1311cfaade3690f04ad052b7400c
alsdk1676 <alsdk6761@gmail.com>
2025-03-25 22:34:49 +0900

    university-exhibition-form 달력 작업 중

---
+++ src/main/webapp/assets/js/exhibition/university-exhibition-form.js
@@ -0,0 +1,5 @@
+const fileButton = document.getElementById("file-button");
+const fileInput = document.getElementById("file-input");
+fileButton.addEventListener("click", () => {
+    fileInput.click();
+});
