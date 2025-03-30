cb3d9ca3c3642a8c9b10c6c8834c5935631cc8fc
croissen <croissen214@gmail.com>
2025-03-31 07:48:15 +0900

    ArtDAO & mapper 추가

--- src/main/java/com/app/dao/ArtDAO.java
+++ src/main/java/com/app/dao/ArtDAO.java
@@ -1,8 +1,12 @@
 package com.app.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.app.mybatis.config.MyBatisConfig;
+import com.app.vo.ArtLikeVO;
+import com.app.vo.ArtVO;
 
 public class ArtDAO {
 	public SqlSession sqlSession;
@@ -10,4 +14,40 @@ public class ArtDAO {
  	public ArtDAO() {
  		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
  	}
+ 	
+ 	
+// 	✔ 작품 업로드
+    // 작품 업로드 (전시중)
+    public void insertArtForExhibition(ArtVO artVO) {
+        sqlSession.insert("art.insert", artVO);
+    }
+
+    // 작품 업로드 (전시예정)
+    public void insertArtForUpcomingExhibition(ArtVO artVO) {
+        sqlSession.insert("art.insert", artVO);
+    }
+
+
+// 	✔ 작품 상세보기
+    // 모든 작품 조회
+    public List<ArtVO> selectAllArts() {
+    	return sqlSession.selectList("art.selectAll");
+    }
+    
+    // 특정 작품 조회 (ID로)
+    public ArtVO selectArtById(int id) {
+    	return sqlSession.selectOne("art.selectById", id);
+    }
+ 	
+    
+// 	✔ 좋아요 기능
+    // 작품 좋아요 추가
+    public void insertArtLike(ArtLikeVO artLikeVO) {
+        sqlSession.insert("art.insertArtLike", artLikeVO);
+    }
+    
+    	
+// 	✔ 전시 필터링
+ 	
+ 	
 }
--- src/main/java/com/app/dao/MypageDAO.java
+++ src/main/java/com/app/dao/MypageDAO.java
@@ -1,8 +1,15 @@
 package com.app.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 
 import com.app.mybatis.config.MyBatisConfig;
+import com.app.vo.ArtLikeVO;
+import com.app.vo.AuctionBiddingVO;
+import com.app.vo.AuctionVO;
+import com.app.vo.ReplyVO;
+import com.app.vo.UserVO;
 
 public class MypageDAO {
 	public SqlSession sqlSession;
@@ -10,4 +17,29 @@ public class MypageDAO {
  	public MypageDAO() {
  		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
  	}
+ 	
+    // 회원 정보 조회
+    public UserVO selectUserDetails(int userId) {
+        return sqlSession.selectOne("user.selectUserDetails", userId);
+    }
+    
+    // 내 댓글 조회
+    public ReplyVO selectReplyById(int id) {
+        return sqlSession.selectOne("selectReplyById", id);
+    } 
+    
+    // 내가 눌렀던 작품 좋아요 조회
+    public List<ArtLikeVO> selectLikesByUserId(int userId) {
+        return sqlSession.selectList("selectLikesByUserId", userId);
+    }
+    
+    // 유저의 경매 구매 내역 조회
+    public List<AuctionBiddingVO> selectBiddingByUserId(int userId) {
+        return sqlSession.selectList("selectBiddingByUserId", userId);
+    }
+
+    // 유저의 경매 판매 내역 조회
+    public List<AuctionVO> selectAuctionByUserId(int userId) {
+        return sqlSession.selectList("selectAuctionByUserId", userId);
+    }
 }
