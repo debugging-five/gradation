9165439c999592bc7266e99f43c6bf4acd28bec4
alsdk1676 <alsdk6761@gmail.com>
2025-03-31 17:25:25 +0900

    university-exhibition-form alert, 팝업

--- src/main/webapp/assets/js/exhibition/university-exhibition-form-alert1.js
+++ src/main/webapp/assets/js/exhibition/university-exhibition-form-alert1.js
@@ -1,3 +1,9 @@
-/**
- * 
- */
+const popup = document.getElementById("popup-wrapper");
+const overlay = document.getElementById("overlay");
+const okButton = document.getElementById("alert-ok-button");
+const cancelButton = document.getElementById("alert-cancel-button");
+
+const openPopup = () => {
+	popup.style.display = "block";
+	overlay.style.display = "block";
+}
