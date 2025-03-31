2928ef9217462b93aabb35adbda1a270a1d25844
sehyunf <ssam010925@gmail.com>
2025-03-31 15:31:40 +0900

    pull project

--- src/main/java/com/app/mypage/controller/MypageMainController.java
+++ src/main/java/com/app/mypage/controller/MypageMainController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.app.Action;
 import com.app.Result;
@@ -13,6 +14,7 @@ public class MypageMainController implements Action{
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
 		Result result = new Result();
+		HttpSession session = req.getSession();
 		result.setPath("mypage-main.jsp");
 		return result;
 	}
